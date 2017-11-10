4ea59eac89f4660dd4c93d59280443b9c535bb08
Kelley Robinson <krobinson@twilio.com>
2017-11-10 14:56:57 -0800

    Experiment

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -6,8 +6,8 @@ import scala.io.StdIn.readLine
 object Main extends App {
   val config = ConfigFactory.load()
   val greeting = config.getString("my.special.greeting")
-
   val name = readLine("What is your name? ")
 
-  println(s"${greeting}, ${name}!")
+  // val fullGreeting = s"${greeting}, ${name}!"
+  println("hello")
 }
