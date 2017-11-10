3542cdb9dbe37cbf7387df23b36f8782164b95d5
Kelley Robinson <krobinson@twilio.com>
2017-11-10 14:58:06 -0800

    now?

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -8,6 +8,5 @@ object Main extends App {
   val greeting = config.getString("my.special.greeting")
   val name = readLine("What is your name? ")
 
-  // val fullGreeting = s"${greeting}, ${name}!"
   println("hello")
 }
